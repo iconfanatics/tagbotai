89acece7fe058eb77e5cb7f41430868e74fe1e01
Sany khan <momin879@gmail.om>
2026-02-24 18:54:45 +0600

    Readme file updated

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,16 +1,24 @@
 import { PrismaClient } from "@prisma/client";
+import { createClient } from "@libsql/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
 
 declare global {
   // eslint-disable-next-line no-var
-  var prismaGlobal: PrismaClient;
+  var prismaGlobal: PrismaClient | undefined;
 }
+
+const libsql = createClient({
+  // Use Turso edge URL from Env, fallback to local SQLite file for development
+  url: process.env.TURSO_DATABASE_URL || "file:dev.sqlite",
+  authToken: process.env.TURSO_AUTH_TOKEN,
+});
+
+const adapter = new PrismaLibSQL(libsql);
+
+const prisma = global.prismaGlobal ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
-  if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient();
-  }
+  global.prismaGlobal = prisma;
 }
-
-const prisma = global.prismaGlobal ?? new PrismaClient();
 
 export default prisma;
