691032958ac755e43ecef32a9af348bbf7687764
Sany khan <momin879@gmail.om>
2026-02-26 05:26:02 +0600

    Fix Vercel 500 lambda error by removing static @libsql import while retaining __db__ singleton

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaLibSQL } from "@prisma/adapter-libsql";
-import { createClient } from "@libsql/client";
 
 declare global {
   // eslint-disable-next-line no-var
@@ -8,12 +7,11 @@ declare global {
 }
 
 if (!global.__db__) {
-  const libsql = createClient({
+  const adapter = new PrismaLibSQL({
     url: process.env.TURSO_DATABASE_URL || "file:dev.sqlite",
     authToken: process.env.TURSO_AUTH_TOKEN,
   });
 
-  const adapter = new PrismaLibSQL(libsql);
   global.__db__ = new PrismaClient({ adapter });
 }
 
